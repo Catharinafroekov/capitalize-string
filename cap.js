ad9b43586b3b74e26e69f323ccdd45144e609a39
Catharina <catharina2002@gmail.com>
2024-10-04 11:14:11 +0200

    split pg join

--- cap.js
+++ cap.js
@@ -2,6 +2,13 @@
 const third = (aName.substring(0,2).toLowerCase() + aName.substring(2, 3).toUpperCase() + aName.substring(3).toLowerCase());
 console.log (third) */
 
-const aName = "catharina";
+/* const aName = "catharina";
 const third = (aName.substring(0,1).toUpperCase() + aName.substring(1).toLowerCase());
-console.log (third)
+console.log (third) */
+
+let theText = "Kaj og Andrea spiser solsikkefrø";
+const modifiedText = theText.replaceAll("spiser", "fråder");
+/* console.log(modifiedText);
+ */
+const splitText = theText.split(" ");
+console.log(splitText.join(" "));
