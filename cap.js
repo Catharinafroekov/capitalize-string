fc2a1457e8f2c732766892eb993166bcc5af4c03
Catharina <catharina2002@gmail.com>
2024-10-04 11:07:26 +0200

    delopgave

--- cap.js
+++ cap.js
@@ -1,3 +1,7 @@
-const aName = "Catharina";
+/* const aName = "Catharina";
 const third = (aName.substring(0,2).toLowerCase() + aName.substring(2, 3).toUpperCase() + aName.substring(3).toLowerCase());
+console.log (third) */
+
+const aName = "catharina";
+const third = (aName.substring(0,1).toUpperCase() + aName.substring(1).toLowerCase());
 console.log (third)
